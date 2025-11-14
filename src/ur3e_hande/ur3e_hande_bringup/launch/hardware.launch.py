89064e1c20df3cbe3b5a9239cc486f5771a9c5d8
Clinton Enwerem <enwerem@terpmail.umd.edu>
2025-11-14 17:25:42 -0500

    hwbringup

--- src/ur3e_hande/ur3e_hande_bringup/launch/hardware.launch.py
+++ src/ur3e_hande/ur3e_hande_bringup/launch/hardware.launch.py
@@ -14,13 +14,26 @@ Components launched:
 """
 
 import os
+from os import path
+from os.path import expanduser
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, TimerAction, IncludeLaunchDescription, ExecuteProcess
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
 
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    TextSubstitution,
+    PythonExpression,
+    EnvironmentVariable
+)
+from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 
@@ -28,27 +41,81 @@ def generate_launch_description():
 
     # launch arguments
     use_perception = LaunchConfiguration("use_perception")
+    description_package = LaunchConfiguration("description_package")
+    description_filepath = LaunchConfiguration("description_filepath")
     obj_pos = LaunchConfiguration("obj_pos")
     robot_ip = LaunchConfiguration("robot_ip")
     launch_rviz = LaunchConfiguration("launch_rviz")
+    launch_ur_rviz = LaunchConfiguration("launch_ur_rviz")
+    use_moveit = LaunchConfiguration("use_moveit")
+    ur_type = LaunchConfiguration("ur_type")
+    use_tool_communication = LaunchConfiguration("use_tool_communication")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
     pnp = LaunchConfiguration("pnp")
     print_metrics = LaunchConfiguration("print_metrics")
     max_acc_scale = LaunchConfiguration("max_acc_scale")
     max_vel_scale = LaunchConfiguration("max_vel_scale")
     goal_pos_tol = LaunchConfiguration("goal_pos_tol")
     goal_ori_tol = LaunchConfiguration("goal_ori_tol")
+    rviz_config = LaunchConfiguration("rviz_config")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    log_level = LaunchConfiguration("log_level")
+
 
     declared_arguments = [
         DeclareLaunchArgument(
             "use_perception",
-            default_value="true",
-            description="Use perception module to detect object pose.\n"
-                        "If false, you must supply obj_pos."
+            default_value="false",
+            description="Use perception module to detect object pose."
+        ),
+        DeclareLaunchArgument(
+            "ur_type",
+            description="Type/series of used UR robot.",
+            choices=[
+                "ur3",
+                "ur3e",
+                "ur5",
+                "ur5e",
+                "ur7e",
+                "ur10",
+                "ur10e",
+                "ur12e",
+                "ur16e",
+                "ur15",
+                "ur20",
+                "ur30",
+            ],
+            default_value="ur3e",
+        ),
+        DeclareLaunchArgument(
+            "use_mock_hardware",
+            default_value="false",
+            description="Start robot with mock hardware mirroring command to its states.",
+        ),
+        DeclareLaunchArgument(
+            "use_tool_communication",
+            default_value="false",
+            description="Start robot with mock hardware mirroring command to its states.",
+        ),
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Use simulated time.",
+        ),
+        DeclareLaunchArgument(
+            "use_moveit",
+            default_value="false",
+            description="Whether to start MoveIt.",
         ),
          DeclareLaunchArgument(
             "max_acc_scale",
             default_value="0.25",
             description="Acceleration scaling for MoveIt2 trajectories.",
+        ),
+        DeclareLaunchArgument(
+            "log_level",
+            default_value="warn",
+            description="ROS 2 log level.",
         ),
         DeclareLaunchArgument(
             "max_vel_scale",
@@ -64,6 +131,16 @@ def generate_launch_description():
             "goal_ori_tol",
             default_value="0.01",
             description="Orientation tolerance for MoveIt2 goals (radians).",
+        ),
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="ur3e_hande_description",
+            description="Package with robot description.",
+        ),
+        DeclareLaunchArgument(
+            "description_filepath",
+            default_value=path.join("urdf", "ur3e_hande_hw.urdf.xacro"),
+            description="Path to URDF/Xacro file, relative to share of `description_package`.",
         ),
         DeclareLaunchArgument(
             "pregrasp_z_offset",
@@ -91,6 +168,11 @@ def generate_launch_description():
             description="Manual (x y z) object position (used only when use_perception=false)."
         ),
         DeclareLaunchArgument(
+            "cam_loc",
+            default_value="right",
+            description="Location of camera wrt robot base frame. \n right: -0.30 0.50 0.10; left: 0.30 0.50 0.10"
+        ),
+        DeclareLaunchArgument(
             "robot_ip",
             default_value="192.168.77.22",
             description="IP address of the UR3e robot controller."
@@ -100,13 +182,38 @@ def generate_launch_description():
             default_value="true",   
             description="Launch RViz or not."
         ),
+        DeclareLaunchArgument(
+            "launch_ur_rviz",
+            default_value="false",   
+            description="Launch UR-only RViz or not. Not useful here since we need both descriptions."
+        ),
+        DeclareLaunchArgument(
+            "rviz_config",
+            default_value=path.join(
+                get_package_share_directory("ur3e_hande_description"),
+                "rviz",
+                "view_robot_hw.rviz",
+            ),
+            description="Path to configuration for RViz2.",
+        ),
     ]
 
     # package directories
-    ur3e_bringup_pkg = get_package_share_directory("ur3e_bringup")
+    ur3e_hardware_bringup_pkg = get_package_share_directory("ur_robot_driver")
     moveit_pkg = get_package_share_directory("ur3e_hande_moveit_config")
     perception_pkg = get_package_share_directory("ur3e_hande_perception")
 
+    # robot_description
+
+    robot_description_content = Command([
+        PathJoinSubstitution([FindExecutable(name="xacro")]),
+        " ",
+        PathJoinSubstitution([FindPackageShare(description_package), description_filepath])
+    ])
+
+    robot_description = {
+        "robot_description": ParameterValue(robot_description_content, value_type=str)
+    }
     ld = LaunchDescription(declared_arguments)
 
     # RealSense Bringup
@@ -140,45 +247,59 @@ def generate_launch_description():
             actions=[
                 IncludeLaunchDescription(
                     PythonLaunchDescriptionSource(
-                        os.path.join(ur3e_bringup_pkg, "launch", "hardware.launch.py")
+                        os.path.join(ur3e_hardware_bringup_pkg, "launch", "ur_control.launch.py")
                     ),
                     launch_arguments={
+                        "ur_type": ur_type,
                         "robot_ip": robot_ip,
-                        "launch_rviz": launch_rviz,
+                        "launch_rviz": launch_ur_rviz,
+                        "use_tool_communication": use_tool_communication,
+                        "use_mock_hardware": use_mock_hardware
                     }.items(),
                 )
             ],
         )
     )
 
-    # 3. Hand-E Gripper Nodes (redundant)
-    # ld.add_action(
-    #     TimerAction(
-    #         period=6.0,
-    #         actions=[
-    #             # JointState publisher for visualization and MoveIt sync
-    #             Node(
-    #                 package="robotiq_hande_ros2_driver",
-    #                 executable="gripper_joint_publisher",
-    #                 name="gripper_joint_publisher",
-    #                 output="screen",
-    #             ),
-    #             # Action server for commanding the gripper
-    #             Node(
-    #                 package="robotiq_hande_ros2_driver",
-    #                 executable="gripper_action_server",
-    #                 name="gripper_action_server",
-    #                 output="screen",
-    #                 parameters=[
-    #                     {"robot_ip": robot_ip},
-    #                     {"p_diff_thresh": 5},
-    #                     {"u_diff_thresh": 4},
-    #                     {"f_diff_thresh": 5},
-    #                 ],
-    #             ),
-    #         ],
-    #     )
-    # )
+    # rviz2
+    ld.add_action(
+        TimerAction(
+            period=8.0,
+            actions=[
+                Node(
+                    package="rviz2",
+                    condition=IfCondition(launch_rviz),
+                    executable="rviz2",
+                    output="log",
+                    arguments=[
+                        "--display-config",
+                        rviz_config,
+                        "--ros-args",
+                        "--log-level",
+                        log_level,
+                    ],
+                    parameters=[{"use_sim_time": use_sim_time}],
+                )
+                ])
+    )
+
+    # rsp
+    ld.add_action(
+        TimerAction(
+            period=6.0,
+            actions=[
+                Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                arguments=["--ros-args", "--log-level", log_level],
+                parameters=[
+                    robot_description,
+                    {"publish_frequency": 50.0, "frame_prefix": "", "use_sim_time": use_sim_time},
+                ],
+            ),
+            ])
+    )
 
     # Perception Nodes
     ld.add_action(
@@ -192,11 +313,14 @@ def generate_launch_description():
                     name="pc_voxel_filter_node",
                     output="screen",
                     parameters=[
-                        {"use_sim_time": False},
-                        {"input_topic": "/camera/camera/depth/color/points"},
-                        {"output_topic": "/filtered_cloud"},
-                        {"leaf_size": 0.005},
-                        {"crop_enabled": True},
+                    {"use_sim_time": use_sim_time},
+                    {"input_topic": "/camera/camera/depth/color/points"},
+                    {"output_topic": "/filtered_cloud"},
+                    {"leaf_size": 0.005},
+                    {"crop_enabled": True},
+                    {"stop_after_first": False}, # keep processing point clouds since cloud is sparser in real life
+                    {"crop_bounds": [0.0, 1.22, 0, 0.51, 0.00, 1.5]},  # xmin, xmax, ymin, ymax, zmin, zmax; Point cloud bounds: x[0.00, 1.22], y[0.00, 0.51], z[0.00, 1.50]
+
                     ],
                 ),
                 Node(
@@ -205,13 +329,16 @@ def generate_launch_description():
                     name="pc_segmentation_node",
                     output="screen",
                     parameters=[
-                        {"use_sim_time": False},
+                        {"use_sim_time": use_sim_time},
                         {"input_topic": "/filtered_cloud"},
-                    ],
+                        {"base_frame": "base_link"},
+                        {"camera_frame": "camera_depth_frame"},
+                        {"stop_after_first_pub": False}, # keep publishing segmentation results
+                        ],
                 ),
                 Node(
                     package="ur3e_hande_perception",
-                    executable="obj_pose_action_server.py",
+                    executable="obj_pose_action_server",
                     name="object_pose_server",
                     output="screen",
                 ),
@@ -223,6 +350,7 @@ def generate_launch_description():
     ld.add_action(
         TimerAction(
             period=12.0,
+            condition=IfCondition(use_moveit),
             actions=[
                 IncludeLaunchDescription(
                     PythonLaunchDescriptionSource(
@@ -245,26 +373,9 @@ def generate_launch_description():
             actions=[
                 Node(
                     package="ur3e_hande_moveit_py",
-                    executable="pnp_demo.py",
+                    executable="pnp_demo",
                     name="pnp_demo",
                     output="screen",
-                )
-            ],
-        )
-    )
-
-    # manual obj_pos mode
-    ld.add_action(
-        TimerAction(
-            period=12.0,
-            condition=UnlessCondition(use_perception),
-            actions=[
-                Node(
-                    package="ur3e_hande_moveit_py",
-                    executable="pnp_demo.py",
-                    name="pnp_demo",
-                    output="screen",
-                    parameters=[{"obj_pos": obj_pos}],
                 )
             ],
         )
